983602f6de710f3e1ce451a5350dcf00354b756b
uliana2006eskova <45653414+uliana2006eskova@users.noreply.github.com>
2019-10-27 22:54:55 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -1,24 +1,40 @@
+"""
+importing requests for getting html-code and bs4 for printing this code in the
+console pretty
+"""
 import requests
 from bs4 import BeautifulSoup
 
 class CinemaParser:
+    """
+    class for parsing cinema
+    """
     def __init__(self, city):
+        """
+        initisalisation
+        """
         self.city = city
         self.content = ""
 
-    def extract_raw_content(self, url):
-        response = requests.get(url)
-        try:
-            self.content = response.content.decode("utf-8",errors='ignore')
-        except:
-            self.content = response.content.decode("cp1251",errors='ignore')
+    def extract_raw_content(self):
+        """
+        function for getting html code
+        """
+        if self.city == "msk":
+            response = requests.get("https://msk.subscity.ru")
+        elif self.city == "spb":
+            response = requests.get("http://spb.subscity.ru")
+        self.content = response.content.decode("utf-8", errors='ignore')
         return self.content
 
     def print_raw_content(self):
+        """
+        function for printing html code
+        """
         soup = BeautifulSoup(''.join(self.content))
         print(soup.prettify())
 
 
-cinema = CinemaParser("msk")
-cinema.extract_raw_content("https://vk.com")
-cinema.print_raw_content()
+CINEMA = CinemaParser("msk")
+CINEMA.extract_raw_content()
+CINEMA.print_raw_content()
