27e0cd7750017b403f5f00151b35297bba0c8ee7
uliana2006eskova <45653414+uliana2006eskova@users.noreply.github.com>
2019-10-27 22:20:53 +0300

    update core.py
    add functions of printing html-code and downloading html-code of the main page

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -1,1 +1,24 @@
-#it is for first commit
+import requests
+from bs4 import BeautifulSoup
+
+class CinemaParser:
+    def __init__(self, city):
+        self.city = city
+        self.content = ""
+
+    def extract_raw_content(self, url):
+        response = requests.get(url)
+        try:
+            self.content = response.content.decode("utf-8",errors='ignore')
+        except:
+            self.content = response.content.decode("cp1251",errors='ignore')
+        return self.content
+
+    def print_raw_content(self):
+        soup = BeautifulSoup(''.join(self.content))
+        print(soup.prettify())
+
+
+cinema = CinemaParser("msk")
+cinema.extract_raw_content("https://vk.com")
+cinema.print_raw_content()
