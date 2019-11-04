e470b975cc14fbd14ebd18524d0a46d0e19b90db
uliana2006eskova <45653414+uliana2006eskova@users.noreply.github.com>
2019-11-04 16:25:22 +0300

    add function for getting film list

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -34,7 +34,19 @@ class CinemaParser:
         soup = BeautifulSoup(''.join(self.content))
         print(soup.prettify())
 
+    def get_film_list(self):
+        """
+        function for getting film list
+        """
+        film_list = []
+        soup = BeautifulSoup(self.content, 'lxml')
+        spans = soup.select('.movie-plates')
+        for _ in spans:
+            now = soup.select('.movie-plate')
+            for nows in now:
+                film_list.append(nows.get('attr-title'))
 
 CINEMA = CinemaParser("msk")
 CINEMA.extract_raw_content()
-CINEMA.print_raw_content()
+#CINEMA.print_raw_content()
+print(CINEMA.get_film_list())
